072fbed8d28ef4766b9dbd2b73638f4585a096f9
liam-sy <76115918+liam-sy@users.noreply.github.com>
2021-01-05 16:35:22 -0500

    Update address filter.py

--- Amish Checker/address filter.py
+++ Amish Checker/address filter.py
@@ -1,41 +1,41 @@
 import csv
 
+from file_picker import pa_i, pa_o, ca_i, ca_o
+
 raw_list=[]
 x=0
 
-open("files/new smicksburg.csv", "w").close()
+#Clear the output document
+pa_o.truncate(0)
 
-s1=open("files/Terr 089- (Smicksburg) - All Data Compiled.csv")
-raw_data=csv.DictReader(s1)
+raw_data=csv.DictReader(pa_i)
 for row in raw_data:
     raw_list.append(row['Street Address'])
-with open("files/new smicksburg.csv", "a+") as s2:
-    s2.write("Address\n")
-    for row in raw_list:
-        raw_list_string=str(raw_list[x])
-        s2.write(str(raw_list_string)+"\n")
-        x+=1
+pa_i.close()
+pa_o.write("Address\n")
+for row in raw_list:
+    raw_list_string=str(raw_list[x])
+    pa_o.write(str(raw_list_string)+"\n")
+    x+=1
 
-s1.close()
-s2.close()
+pa_o.close()
 
-#//////////// ^ Smicksburg above /// Amish below v /////////////#
+#//////////// ^ Territory Above /// Amish below v /////////////#
 
 raw_list=[]
 x=0
 
-open("files/new confirmed amish.csv", "w").close()
+#Clear the output document
+ca_o.truncate(0)
 
-a1=open("files/Contact Filtered Export Contacts (4).csv")
-raw_data=csv.DictReader(a1)
+raw_data=csv.DictReader(ca_i)
 for row in raw_data:
     raw_list.append(row['Address'])
-with open("files/new confirmed amish.csv", "a+") as a2:
-    a2.write("Address")
-    for row in raw_list:
-        raw_list_string=str(raw_list[x])
-        a2.write((str(raw_list_string)+" \n"))
-        x+=1
+ca_i.close()
+ca_o.write("Address")
+for row in raw_list:
+    raw_list_string=str(raw_list[x])
+    ca_o.write(str(raw_list_string)+"\n")
+    x+=1
 
-a1.close()
-a2.close()
+ca_o.close()
