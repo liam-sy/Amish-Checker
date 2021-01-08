576532d9b8b071f7db1bd24f8d35846b4113c1c1
liam-sy <76115918+liam-sy@users.noreply.github.com>
2021-01-08 10:26:04 -0500

    Update file_picker.py

--- file_picker.py
+++ file_picker.py
@@ -1,7 +1,7 @@
-# Below are the original input files, "Confirmed Amish Input" and "Potential Amish Input" respectively
-ca_i = open("files/Contact Filtered Export Contacts (4).csv", "r+")
-pa_i = open("files/Terr 089- (Smicksburg) - All Data Compiled.csv", "r+")
+# Below are are the territory (potential amish) input and output docs
+pa_i = open("files/Terr 140 - Stump Creek - All names.csv", "r+")
+pa_o = open("files/new stump creek.csv", "r+")
 
-# Below are the output files, following the same naming convention
+# Below are are the confirmed amish input and output docs
+ca_i = open("files/Contact Filtered Export Contacts (4).csv", "r+")
 ca_o = open("files/new confirmed amish.csv", "r+")
-pa_o = open("files/new smicksburg.csv", "r+")
